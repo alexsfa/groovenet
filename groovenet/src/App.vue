7d9ea7df4bac5cc7eaa9661e9ddf88591adac329
Sfa <alexandrossfak000@gmail.com>
2024-11-30 16:11:16 +0200

    login,signup and home static pages

--- groovenet/src/App.vue
+++ groovenet/src/App.vue
@@ -1,85 +1,72 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  computed: {
+    showBannerFooter() {
+      return (this.$route.name !== 'signup') && (this.$route.name !== 'login');
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div>
+  <nav v-if="!$route.meta.hideNav" class="sticky top-0 left-0 bg-gray-800 py-2 px-2 ">
+        <header  class="max-w-10xl">
+            <div class="flex items-center justify-between">
+                <div class="menu-left items-center flex space-x-8">
+                  <a href="#">
+                      <img src="../src/assets/images/Groovenet-logo.png" width="50px" height="50px">
+                  </a>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+                  <form >
+                    <div class="bg-orange-200 rounded-3xl h-9 w-80 flex items-center">
+                        <img src="../src/assets/icons/search-icon.svg" class="h-9 w-9 pl-1.5 pt-0.5">
+                        <input id="myInput" class="search-input w-max bg-orange-200 text-xl text-orange-750 border-0 outline-none font-geologica pl-2" type="search" >
+                    </div>
+                  </form>
+                </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+                <div class="menu-center pr-40 flex space-x-8">
+                      <a href="#">
+                        <h1 class="text-orange-400 text-lg font-geologica hover:scale-110 transition-transform">Notifications</h1>
+                      </a>
+
+                      <a href="#">
+                        <h1 class="text-orange-400 text-lg font-geologica hover:scale-110 transition-transform">Profile</h1>                              
+                      </a>
+
+                      <a href="#">
+                        <h1 class="text-orange-400 text-lg font-geologica hover:scale-110 transition-transform">Favorites</h1>                              
+                      </a>
+
+                      <router-link to="/login">
+                        <h1 class="text-orange-400 text-lg font-geologica hover:scale-110 transition-transform">Log out</h1>                                                         
+                      </router-link>
+                    
+                </div>
+
+                <div class="menu-right pl-12 pr-12 flex items-center justify-between">
+                    <button class="bg-orange-400 w-10 h-11 text-black rounded-xl  hover:bg-orange-750 flex items-center">
+                      <img src="../src/assets/icons/upload-svgrepo-com.svg" class="h-9 w-9 pl-1">
+                    </button>
+                    <button></button>
+                </div>
+            
+            </div>  
+          </header>
+    </nav>
+
+    <main class="px-8 py-6 bg-gray-800">
+      <RouterView />
+    </main>
+
+    <footer v-if="!$route.meta.hideNav" class="flex sticky bottom-0 left-0 w-full h-16 bg-gray-800 ">
+          <img src="@/assets/images/groovenet-full-logo.png" class='transform scale-75 origin-bottom -ml-12 -mt-6 '>
+    </footer>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- groovenet/tailwind.config.js
+++ groovenet/tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme:
+     {
+      colors: {
+        'gray': {
+          800: '#3A3A3A'
+        },
+        'orange': {
+          200: '#F8E4AE',
+          400: '#FBC12A',
+          750: '#997000'
+        },
+        'black': '#000000'
+      },
+      fontFamily: {
+        geologica: ["Geologica"]
+      }
+      
+    },
+    plugins: [require('tailwind-scrollbar-hide')]
 }
+
 
