efe1ae85399a44f5c9063f6bf558f181d7b589e1
Sfa <alexandrossfak000@gmail.com>
2024-12-13 16:53:54 +0200

    testing sign up

---
+++ groovenet_frontend/src/views/FavoritesView.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="max-w-7xl h-[775px] mx-auto h-screen ">
+        <div class="grid grid-cols-[175px_175px_175px_175px_175px_175px_175px] gap-6 justify-center">
+            <div class=" bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/ink-days.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Morphing jar</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Ink Days</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/banity.png" class="ml-6 my-3 w-32 h-32 rounded-2xl object-cover">
+                <div class="pb-2 grid grid-rows-[50px_20px] justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Seaside Hotel<br>Room Fantasy</h1>
+                    <h3 class="font-geologica text-orange-200 text-sm">Banity</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/dreamland.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Half Twin Brother</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Dreamland</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/harm.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Messier13</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Harm</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/better-without-me.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Desolation row</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Better without me</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/one-star.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Half Twin Brother</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">One star</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/218.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Jasper Project</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">218</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/failsafe.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Slother</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Failsafe</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/hummingbirds.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 leading-6 text-lg">Pavlos Paloka</h1>
+                    <h3 class="font-geologica text-orange-200 text-md">Hummingbirds</h3>
+                </div>
+            </div>
+
+            <div class="bg-orange-750 hover:bg-orange-800 rounded-3xl">
+                <img src="@/assets/images/cover-pictures/thelw-na-vgw.jpg" class="ml-6 my-3 w-32 h-32 rounded-2xl">
+                <div class="pb-2 grid grid-rows-2 justify-items-center items-center ">
+                    <h1 class="font-geologica text-orange-200 text-center leading-6 text-lg">Polyxrwmh<br> anasfaleia</h1>
+                    <h3 class="font-geologica text-orange-200 text-center text-sm">thelw na vgw<br>me xara apo edw</h3>
+                </div>
+            </div>
+        </div>
+    </div> 
+</template>
